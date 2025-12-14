dd911e627408a318a0da93d3e151605156bb0463
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2025-12-14 05:12:50 +0000

    Add status endpoint

---
+++ src/routes/status.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { HttpStatusCodes } from '../utils/http.js';
+import { toSafeInteger } from '../utils/number.js';
+
+const MIN_VALID_STATUS_CODE = 200;
+const MAX_VALID_STATUS_CODE = 599;
+
+const statusRouter = Router();
+
+statusRouter.all('/:status', (req, res) => {
+  const statusCode = toSafeInteger(req.params.status);
+  const isValidStatusCode =
+    statusCode !== undefined && statusCode >= MIN_VALID_STATUS_CODE && statusCode <= MAX_VALID_STATUS_CODE;
+
+  if (!isValidStatusCode) {
+    res.status(HttpStatusCodes.BAD_REQUEST).json({
+      error: {
+        message: `Invalid status code. Must be an integer between ${MIN_VALID_STATUS_CODE} and ${MAX_VALID_STATUS_CODE}.`,
+      },
+    });
+    return;
+  }
+
+  res.sendStatus(statusCode);
+});
+
+export { statusRouter };
