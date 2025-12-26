d6378cbdb8d95624dc6fd049c9dd59a5b859034a
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2025-12-26 13:16:45 +0000

    add pino

---
+++ src/logger.ts
@@ -0,0 +1,29 @@
+import pino from 'pino';
+import { environment } from './env.js';
+
+const logger = pino(
+  environment.nodeEnv === 'development'
+    ? {
+        level: environment.logLevel,
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        },
+      }
+    : {
+        level: environment.logLevel,
+      },
+);
+
+export const log = {
+  fatal: logger.fatal.bind(logger),
+  error: logger.error.bind(logger),
+  warn: logger.warn.bind(logger),
+  info: logger.info.bind(logger),
+  debug: logger.debug.bind(logger),
+  trace: logger.trace.bind(logger),
+};
