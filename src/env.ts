77d5a58160e6fd9e3ac95cf66a3ea19021d23e50
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-01-22 13:46:07 +0000

    Improve environment variable validation and assignment

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,26 @@
 import { toSafeInteger } from './utils/number.js';
 
+const logLevel = process.env['LOG_LEVEL'] ?? 'info';
+if (!['trace', 'debug', 'info', 'warn', 'error', 'fatal'].includes(logLevel)) {
+  throw new Error(
+    `Invalid configuration: LOG_LEVEL (${logLevel}) must be one of: trace, debug, info, warn, error, fatal.`,
+  );
+}
+
+const nodeEnv = process.env['NODE_ENV'] ?? 'development';
+if (!['development', 'production', 'test'].includes(nodeEnv)) {
+  throw new Error(
+    `Invalid configuration: NODE_ENV (${nodeEnv}) must be one of: development, production, test.`,
+  );
+}
+
+const port = toSafeInteger(process.env['PORT']) ?? 8000;
+if (port < 0 || port > 65535) {
+  throw new Error(`Invalid configuration: PORT (${port}) must be between 0 and 65535.`);
+}
+
 export const environment = {
-  logLevel: process.env['LOG_LEVEL'] || 'info',
-  nodeEnv: process.env['NODE_ENV'] || 'development',
-  port: toSafeInteger(process.env['PORT']) || 8000,
+  logLevel: logLevel as 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal',
+  nodeEnv: nodeEnv as 'development' | 'production' | 'test',
+  port,
 };
