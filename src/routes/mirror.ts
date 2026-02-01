015f994bc6f37caa9f09547cb9f7190f2e6c9fbc
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-02-01 13:10:58 +0000

    Update mirror.ts

--- src/routes/mirror.ts
+++ src/routes/mirror.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const mirrorRouter = Router();
 
 mirrorRouter.all('/', (req, res) => {
-  res.json(req.body === undefined ? {} : req.body);
+  res.json(req.method === 'GET' || req.body === undefined ? {} : req.body);
 });
 
 export { mirrorRouter };
